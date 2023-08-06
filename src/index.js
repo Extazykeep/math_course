9c6b73b6500e17077b98fb9a43a441c2d8879c5a
Extazykeep <52160853+Extazykeep@users.noreply.github.com>
2023-08-06 19:51:54 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/*
+ * External dependencies
+ */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/modal';
+
+/*
+ * Internal dependencies
+ */
+import './styles/style.scss';
+import form from './components/form';
+
+document.addEventListener('DOMContentLoaded', () => {
+    form();
+});
